de62ab383d996e846c8b08a15c4cf98084923fbf
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-10 11:00:08 +0300

    Add files via upload

---
+++ AnagramCheck/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+Console.WriteLine(AnagramCheck("silent", "listen"));
+File.WriteAllText("../../../text.txt", $"{AnagramCheck("silent", "listen")}");
+static bool AnagramCheck(string first, string second)
+{
+    if (first.Count() != second.Count())
+    {
+        return false;
+    }
+    else
+    {
+        List<char> firstWord = new List<char>(first);
+        List<char> secondWord = new List<char>(second);
+
+        foreach (char c in firstWord)
+        {
+            if (secondWord.Contains(c))
+            {
+                int index = secondWord.IndexOf(c);
+                secondWord.RemoveAt(index);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return secondWord.Count == 0;
+       
+    }
+}
+
+
+//ChatGPT version - not perfect, valid only for letters
+//string str1 = "listen";
+//string str2 = "silent";
+//bool result = AreAnagrams(str1, str2);
+//Console.WriteLine(result);  // Output: True
+
+//string str3 = "112233";
+//string str4 = "123321";
+//bool result2 = AreAnagrams(str3, str4);
+//Console.WriteLine(result2);  // Output: False
+// static bool AreAnagrams(string str1, string str2)
+//{
+//    // Remove any whitespace and convert both strings to lowercase
+//    str1 = str1.Replace(" ", "").ToLower();
+//    str2 = str2.Replace(" ", "").ToLower();
+
+//    // Check if the lengths of the strings are equal
+//    if (str1.Length != str2.Length)
+//    {
+//        return false;
+//    }
+
+//    // Create character frequency counters for both strings
+//    int[] counter1 = new int[26];
+//    int[] counter2 = new int[26];
+
+//    // Update the counters for each character in the first string
+//    foreach (char c in str1)
+//    {
+//        counter1[c - 'a']++;
+//    }
+
+//    // Update the counters for each character in the second string
+//    foreach (char c in str2)
+//    {
+//        counter2[c - 'a']++;
+//    }
+
+//    // Check if the character frequency counters are equal
+//    for (int i = 0; i < 26; i++)
+//    {
+//        if (counter1[i] != counter2[i])
+//        {
+//            return false;
+//        }
+//    }
+
+//    // If all checks passed, the strings are anagrams
+//    return true;
+//}
---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine(string.Join(", ", ArrayRotation(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3)));
+static int[] ArrayRotation(int[] array, int step)
+{
+    int[] resultArray = new int[array.Length];
+ 
+    //for (int i = 0; i < array.Length; i++)
+
+    //{
+    //    if (i + step < array.Length)
+    //    {
+    //       resultArray[i + step] = array[i];          
+           
+    //    }
+    //    else
+    //    {
+    //        Array.Copy(array, i, resultArray, 0, step);
+    //        break;
+    //    }
+
+    //}
+    Array.Copy(array, 0, resultArray, step, array.Length-step);
+    Array.Copy(array, step + 1, resultArray, 0, step);  
+    return resultArray;
+}
