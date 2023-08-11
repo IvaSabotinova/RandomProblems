d48860b6af7f76bb6b1dca6b60c86caf53242469
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-11 10:31:07 +0300

    Add files via upload

---
+++ Fibonacci_Sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int number = int.Parse(Console.ReadLine());
+
+long []array = new long [number];
+
+long firstNumber = 0;
+long secondNumber = 1;
+
+array[0] = firstNumber;
+array[1] = secondNumber;
+
+if (number == 1 || number == 2)
+{
+    Console.WriteLine("Input number should be at least 3");
+}
+else
+{
+    for (int i = 2; i < number; i++)
+    {
+        long resultNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = resultNumber;
+        array[i] = resultNumber;
+    }
+    Console.WriteLine(string.Join(", ", array));
+}
+
+
+
