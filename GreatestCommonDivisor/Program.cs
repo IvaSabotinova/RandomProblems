05ff116ed58147729bb4d716e8dfa1935d566def
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-12 10:19:55 +0300

    Add files via upload

---
+++ GreatestCommonDivisor/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.WriteLine(GreatestCommonDivisor(9,2));
+
+
+static int GreatestCommonDivisor(int a, int b)
+{
+    int remainder;
+
+    while (b != 0)
+    {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+
+    return a;
+
+    //int smaller = a;
+    //if (a > b)
+    //{
+    //    smaller = b;
+    //}
+    //int gcd = 1;
+    //for (int i = 1; i <= smaller; i++)
+    //{
+    //    if (a % i == 0 && b % i == 0)
+    //    {
+    //        gcd = i;
+    //    }
+    //}
+    //if (a == 0)
+    //{
+    //    gcd = b;
+    //}
+    //if (b == 0){
+    //    gcd = a;
+    //}
+    //return gcd;
+    //  return b == 0 ? a : GreatestCommonDivisor(b, a % b);
+}
