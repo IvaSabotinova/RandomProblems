8f53607fccc050769bd724e17a15ea16cded88b4
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-14 01:24:48 +0300

    Add files via upload

---
+++ LowestCommonDivisor/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int p, q, x, y, lcm;
+p = x = 20;
+q = y = 25;
+
+while (x != y)
+{
+    if (x > y)
+        x = x - y;
+    else
+        y = y - x;
+}
+
+lcm = (p * q) / x;
+Console.WriteLine("LCM of " + p + " and " + q + " is: " + lcm);
