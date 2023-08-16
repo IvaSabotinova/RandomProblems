4783d689da17aca889d0434c28c14757c97d4974
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-16 14:01:49 +0300

    Add files via upload

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string input = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(input))
+{
+    Console.WriteLine("String cannot be null or whitespace");
+    return;
+}
+
+char[] output = input.ToCharArray().Reverse().ToArray();
+
+bool isPalindrome = false;
+
+if (input == new string(output))
+{
+    isPalindrome = true;   
+}
+
+Console.WriteLine(isPalindrome);
