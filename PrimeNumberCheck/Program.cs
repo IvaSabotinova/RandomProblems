4b30dfb1fcd3ab119284acf82fa597c96e97c8e7
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-17 13:51:08 +0300

    Add files via upload

---
+++ PrimeNumberCheck/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+Console.WriteLine(IsPrime(19));
+
+static bool IsPrime(int number)
+{
+    bool isPrime = false;
+    int count = 0;
+	if (number > 1)
+	{
+        isPrime = true;
+        for (int i = 1; i <= number; i++)
+        {
+
+            if (number % i == 0)
+            {
+                count++;
+                if (count > 2)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+    }
+  
+	return isPrime;
+}
