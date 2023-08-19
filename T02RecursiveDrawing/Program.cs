5b700bbc047997a0154715e7b024acfcc05ed729
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-19 10:59:39 +0300

    Add files via upload

---
+++ T02RecursiveDrawing/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+PrintFigure(n);
+
+
+static void PrintFigure(int n)
+{
+
+    if (n <= 0)
+    {
+        return;
+    }
+    Console.WriteLine(new string('*', n));
+    PrintFigure(n - 1);
+    Console.WriteLine(new string('#', n));
+}
