e9194d399b63a94eb464e62a229d999435f84d76
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-08-20 12:55:40 +0300

    Add files via upload

---
+++ T03.Generating0_1Vectors/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+GenerateVector(array, 0);
+
+static void GenerateVector(int[] array, int index)
+{
+    if (index == array.Length)
+    {
+        Console.WriteLine(string.Join("", array));
+        return;
+    }
+    for (int i = 0; i <= 1; i++)
+    {
+        array[index] = i;
+        GenerateVector(array, index + 1);
+    }
+}
+
+
+
+
